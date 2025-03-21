b77901bfee83aef352a84bc1335102685b4ccc8f
Da Da <ivan.ivan.parsaev@inbox.ru>
2025-03-21 11:09:39 +0700

    Initial commit

---
+++ CommonElementsProject/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonElementsProject
+{
+    public class Class1
+    {
+    }
+}
